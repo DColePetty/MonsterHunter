bc480490cf1940e5cdbd843286b0d51ef4e5ec47
Cole Petty <dcole.petty@gmail.com>
2018-02-20 21:08:03 -0800

    Initial commit

---
+++ Assets/Scripts/Combat.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Combat : MonoBehaviour {
+    public float health = 100f;
+    public Slider sliderV;
+
+    private void Update()
+    {
+        IncrementHealth(3);
+        UpdateSlider();
+    }
+    private void Start()
+    {
+        UpdateSlider();
+        //sliderV.value = health;
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        Debug.Log(collision);
+        //if (collision)
+        health -= (50f * Time.deltaTime); Debug.Log(health);
+        //sliderV.value = health;
+        UpdateSlider();
+        if (health < 0 ) Destroy(gameObject);
+    }
+    /*
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        Debug.Log(collision);
+        gameObject.GetComponent<Rigidbody2D>().velocity = Vector3.zero;
+        demonHealth -= (10f * Time.deltaTime); Debug.Log(demonHealth); 
+        if (demonHealth < 0) Destroy(gameObject);
+    }
+    */
+
+    public void IncrementHealth(int n)
+    {
+        if(health < 250 && tag == "Player") health += n * Time.deltaTime;
+    }
+    private void UpdateSlider()
+    {
+        sliderV.value = health;
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+   
+    private float moveSpeed = 5f;
+    // Use this for initialization
+    //void Start () {	
+    //}
+
+    // Update is called once per frame
+    void Update () {
+        //Input.GetKeyDown is on the down press ..up arrow
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += Vector3.up * moveSpeed * Time.deltaTime;
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            transform.eulerAngles = new Vector3(0, 0, 180);
+        }
+        //left arrow
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += Vector3.left * moveSpeed * Time.deltaTime;
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.eulerAngles = new Vector3(0,0,-90);
+        }
+        //right arrow
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += Vector3.right * moveSpeed * Time.deltaTime;
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+         {
+           transform.eulerAngles = new Vector3(0, 0, 90);
+         }
+        // down arrow
+            if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position += Vector3.down * moveSpeed * Time.deltaTime;
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            transform.eulerAngles = new Vector3(0, 0, 360);
+        }
+        
+    }
+    // add in scirpt for diagonal movement if both say left and up are pressed at the same time 
+}
